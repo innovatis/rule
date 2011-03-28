9b83e33bad43f2974fec232cf9a65456a963fc9b
Burke Libbey <burke@burkelibbey.org>
2011-03-28 14:17:14 -0500

    foo

---
+++ lib/rule/assertions.rb
@@ -0,0 +1,21 @@
+module Rule
+  module Assertions
+    def assert(assertion, message)
+      unless assertion
+        add_error message
+      end 
+    end 
+    
+    def assert_presence_of(thing, name)
+      unless thing.present?
+        add_error "#{name} must be provided"
+      end 
+    end 
+
+    def assert_absence_of(thing, name)
+      unless thing.blank?
+        add_error "#{name} must be blank"
+      end 
+    end 
+  end 
+end 
